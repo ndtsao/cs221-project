5e802fc4e7291e453d8d5ddc3c1b227d4e878f79
Andy Tsao <adtsao@gmail.com>
2017-11-22 18:58:40 -0800

    Silenced some pylint warnings

--- src/util.py
+++ src/util.py
@@ -1,12 +1,12 @@
 """ Useful helper functions """
 import re
 import csv
-import string
+# import string
 import collections
 from nltk.corpus import stopwords
 import nltk
-from nltk.corpus import wordnet
-from nltk.tokenize import sent_tokenize, word_tokenize
+# from nltk.corpus import wordnet
+# from nltk.tokenize import sent_tokenize, word_tokenize
 
 def dot_product(vec1, vec2):
     """
@@ -42,20 +42,20 @@ def word_count(text):
         result[word] += 1
     return result
 
-def removeNonWords(x):
+def remove_non_words(text):
     """
     Remove words that are not in the (English) dictionary
     @param string x
     @return string x with non-English words removed
     Example: 'The Unitedd States' --> 'The States'
     """
-    myString = list(x.split())
+    words = list(text.split())
     wordlist = [w for w in nltk.corpus.words.words('en') if w.islower()]
 
-    filtered_words = [word for word in myString if word in wordlist]
+    filtered_words = [word for word in words if word in wordlist]
     return ' '.join(filtered_words)
 
-def removeStopWordsString(x):
+def remove_stop_words(text):
     """
     Remove stop words from a string.
     @param string x
@@ -63,12 +63,12 @@ def removeStopWordsString(x):
     Example: 'The United States' --> {'United': 1, 'States': 1}
     """
     stop_words = set(stopwords.words('english'))
-    l = list(x.split())
+    words = list(text.split())
 
     for stop_word in stop_words:
-        l = [word for word in l if word != stop_word]
+        words = [word for word in words if word != stop_word]
 
-    return ' '.join(l)
+    return ' '.join(words)
 
 def normalize(vec):
     """
@@ -100,21 +100,21 @@ def word_pair_correl(texts):
     for (idx1, (word1, count1)) in enumerate(all_word_counts.items()):
         for idx2 in range(idx1 + 1, len(all_word_counts.items())):
             word2, count2 = all_word_counts.items()[idx2]
-            result[(word1, word2)] = float(sum([counts[word1] * counts[word2]
-                for counts in word_counts])) / (count1 * count2)
+            result[(word1, word2)] = float(sum([counts[word1] * counts[word2] \
+                    for counts in word_counts])) / (count1 * count2)
     return result
 
-def loadArticles(csvPath, headlineCol, articleCol, dateCol, delSC = True,
-        rnw = False):
+def load_articles(csv_path, cols, del_special_chars=True, rem_non_words=False):
     """
     Load all articles from a given file
     """
     articles = []
-    headlines = []
-    dates = []
+    summaries = []
+    topics = []
+    topic_col, summary_col, article_col = cols
 
-    with open(csvPath, 'rU') as f:
-        reader = csv.reader(f, delimiter=',')
+    with open(csv_path, 'rU') as csv_file:
+        reader = csv.reader(csv_file, delimiter=',')
         next(reader, None) # skip the header
 
         counter = 0
@@ -127,14 +127,14 @@ def loadArticles(csvPath, headlineCol, articleCol, dateCol, delSC = True,
             # kill all non-unicode articles (foreign language -- may want better
             # way to deal with this in the future)
             try:
-                article = row[articleCol]
+                article = row[article_col]
                 article = article.decode('utf-8', 'ignore')
 
                 # tokenize the article
                 # article = sent_tokenize(article)
 
                 # delete numbers
-                if delSC:
+                if del_special_chars:
                     article = re.sub('[^a-zA-Z-_* ]', '', article)
                     # chars_to_remove = '1234567890'
                     # table = {ord(char): None for char in chars_to_remove}
@@ -142,21 +142,21 @@ def loadArticles(csvPath, headlineCol, articleCol, dateCol, delSC = True,
                     # article = [a.translate(table).encode('ascii', 'ignore')
                     #         for a in article]
 
-                if rnw:
-                    article = removeNonWords(article)
-                    # article = [removeNonWords(a) for a in article]
+                if rem_non_words:
+                    article = remove_non_words(article)
+                    # article = [remove_non_words(a) for a in article]
 
                 # temp
                 articles.append(article)
-                headlines.append(row[headlineCol])
-                dates.append(row[dateCol])
+                summaries.append(row[summary_col])
+                topics.append(row[topic_col])
                 # articles.extend(article)
                 # for i in range(len(article)):
-                #     headlines.append(row[headlineCol])
-                #     dates.append(row[dateCol])
+                #     summaries.append(row[summary_col])
+                #     topics.append(row[topic_col])
 
             except UnicodeError:
                 print "non-unicode article"
             counter += 1
 
-    return (articles, headlines, dates)
+    return (topics, summaries, articles)
